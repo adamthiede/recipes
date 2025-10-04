ec6e38d9e21a1b0fe54a38c950444f5f54bcdfcc
adamthiede <me@adamthiede.com>
2025-10-04 13:29:28 -0500

    add stuff

--- build.py
+++ build.py
@@ -2,25 +2,57 @@
 
 import glob
 import os
+import subprocess
 
+MARKDOWN = "/usr/bin/markdown"
+
+# index.html header and footer
 headfile = open("templates/head.html", "r")
 html_head = headfile.read().strip()
 headfile.close()
-
 footfile = open("templates/foot.html", "r")
 html_foot = footfile.read().strip()
 footfile.close()
+# individual recipe header and footer
+rhead = open("templates/recipe_head.html", "r")
+rhead_html = rhead.read().strip()
+rhead.close()
+rfoot = open("templates/recipe_foot.html", "r")
+rfoot_html = rfoot.read().strip()
+rfoot.close()
 
+src_dir = "src"
 gen_dir = "recipes"
 
+if not os.path.exists(gen_dir):
+    os.makedirs(gen_dir)
 
-def gen_index():
-    recipes = ""
-    return recipes
+
+def convert_files():
+    index_list = []
+    for file in glob.glob(f"{src_dir}/*.md"):
+        md_name = os.path.basename(file)
+        md_title = ""
+        with open(file) as f:
+            first_line = f.readline().strip('\n')
+            md_title = first_line.lstrip("# ")
+        html_name = md_name[:-2] + "html"
+        print(f"Writing {html_name} from {md_name}")
+        html_output = subprocess.check_output([MARKDOWN, file])
+        index_list.append(
+            f"<li><a href='{gen_dir}/{html_name}'>{md_title}</a></li>")
+        with open(f"{gen_dir}/{html_name}", "w") as f:
+            str_data = html_output.decode("utf-8")
+            f.write(rhead_html + f"<title>{md_title}</title>" + str_data +
+                    rfoot_html)
+    return index_list
 
 
 def __main__():
-    html = html_head + gen_index() + html_foot
+    index_list = convert_files()
+    index_list.sort()
+    index_list_txt = "\n".join(index_list)
+    html = html_head + "\n" + index_list_txt + "\n" + html_foot
     index = open("index.html", "w")
     index.write(html)
     index.close()
---
+++ src/apple_cake.md
@@ -0,0 +1,27 @@
+# Apple Cake
+
+## Ingredients
+
+- 4 cups peeled, cored, cubed granny smith apples (3 to 4 apples)
+- 1 3/4 cup sugar
+- 2 1/4 cup flour
+- 1 tsp salt
+- 2 tsp baking soda
+- 1 tsp cinnamon
+- 1/2 tsp nutmet
+- 1/2 tsp clove
+- 1/2 cup canola oil
+- 2 beaten eggs
+- 1 package dark chocolate chips
+- 2 tsp vanilla
+
+## Steps
+
+- Cut appples into small bite sized cubes and put into a large mixing bowl.
+- Stir in sugar to evenly coat apples.
+- Stir in dry ingredients (flour, salt, etc)
+- Create a small well or hollow in the bowl and fill it with the eggs, oil, chocolate chips, and vanilla
+- Stir until evenly distributed
+- Press into a well greased baking dish (9x13 inches)
+- Bake at 325F for 45-60 minutes. You will need to watch carefully and check with a toothpick.
+
